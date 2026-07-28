0444e0e98fd52d60498f2ee66f408f041593907c
GabrielT27 <gabrielmarquesterr@gmail.com>
2026-07-28 11:10:29 -0300

    Exibe e implementa carrinho dinâmico com arrays, JSON e localStorage.

--- carrinho.js
+++ carrinho.js
@@ -1,21 +1,51 @@
 
 window.onload = carrinho 
 
-function carrinho () {
-    let nomeProduto = localStorage.getItem("nomeProduto")
-    let qtd = localStorage.getItem("qtd")
-    let preco = localStorage.getItem("preco")
-    let imgProduto = localStorage.getItem("imgProduto")
+function carrinho() {
+
+    let produtos = JSON.parse(
+
+        // transforma o JSON em array novamente
+        // Busca os dados salvos na chave "carrinho" na home.js
+        localStorage.getItem("carrinho")
+
+    ) || [] // Se não existir nada salvo, cria um array vazio
+
+    console.log(produtos)
 
 
 
 
-    document.querySelector(".nomeProduto").innerText = "Nome: " + nomeProduto
-    document.querySelector(".imgProduto").src = imgProduto
-    document.querySelector(".qtd").innerText = "Quantidade: " + qtd
-    document.querySelector(".preco").innerText = "Preço: R$ " + preco
+let listaProdutos = document.querySelector(".listaProdutos")  // Cria uma variável que guarda a referência da div com a classe "listaProdutos" para que ela possa ser manipulada pelo JavaScript
 
+produtos.forEach (produto => {   // ARROW FUNCTION para cada produto dentro do array produtos
+    listaProdutos.innerHTML +=  // Adiciona um novo bloco de HTML dentro da div listaProdutos
+    ` 
+    <div class="imagin">  
+    
+
+    <img src="${produto.imgProduto}">
+
+    <div class="hiscrita">
+
+        <p class="nomeProduto pe">
+            ${produto.nome}
+        </p>
+
+        <p class="qtd pe">
+            ${produto.quantidade}
+        </p>
+
+        <p class="preco pe">
+            ${produto.preco}
+        </p>
+
+
+    </div>
+    </div>
+
+    `
+
+})
 }
-
-
 
